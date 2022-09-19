8558207042c887bf21ab5186e69ad5c70f87dd99
MattBlodgettProjects <mwblodgett2019@gmail.com>
2022-09-19 15:47:36 -0400

    Implement and test for get_valid_moves() in PacMan class

--- Projects/P3/src/PacMan.java
+++ Projects/P3/src/PacMan.java
@@ -14,7 +14,26 @@ public class PacMan {
   }
 
   public ArrayList<Location> get_valid_moves() {
-    return null;
+    ArrayList<Location> possibleMoves = new ArrayList<Location>();
+    ArrayList<Location> validMoves = new ArrayList<Location>();
+
+    possibleMoves.add(this.myLoc.shift(0,1));
+    possibleMoves.add(this.myLoc.shift(1,0));
+    possibleMoves.add(this.myLoc.shift(-1,0));
+    possibleMoves.add(this.myLoc.shift(0,-1));
+
+    //now looping through possible moves and adding the valid moves into new arr
+
+    //for now said that it was an invalid move to go into the ghost. Not sure if 
+    // this is true or not in the real game. 
+    for (int i=0; i < possibleMoves.size(); i++){
+      if ((this.myMap.getLoc(possibleMoves.get(i)) != null) &&
+           !(this.myMap.getLoc(possibleMoves.get(i)).contains(Map.Type.WALL)) &&
+           !(this.myMap.getLoc(possibleMoves.get(i)).contains(Map.Type.GHOST))){
+            validMoves.add(possibleMoves.get(i));
+      }
+    }
+    return validMoves; 
   }
 
   public boolean move() {
