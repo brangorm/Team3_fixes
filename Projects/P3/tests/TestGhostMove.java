b7f36861cf007b196fa740d1aa2f2adb02795802
oliviamd <oliviamd@terpmail.umd.edu>
2022-09-21 19:01:25 -0400

    fixing something in TestGhostMove

--- Projects/P3/tests/TestGhostMove.java
+++ Projects/P3/tests/TestGhostMove.java
@@ -8,7 +8,7 @@ public class TestGhostMove extends TestCase {
     NoFrame frame = new NoFrame(); //Creates A New Map With Walls and Tokens w/o a Display
 
     //Creating Players
-    Ghost ghost = frame.addGhost(new Location(x, y), "name", Color.red); 
+    Ghost ghost = frame.addGhost(new Location(5, 5), "name", Color.red); 
 
     assertTrue(ghost.move());
   }
